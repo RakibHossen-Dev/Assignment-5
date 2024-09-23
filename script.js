7f2eb361b23666574f1b225b584af37ed1af165f
Rakib <hellorakibhossen@gmail.com>
2024-09-22 23:34:19 -0700

    Implemented timeline tracking for user donation

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function amountValueEl(id) {
   return amountValue;
 }
 
+function donationPlaceEl(id) {
+  const placeName = document.getElementById(id).innerText;
+  return placeName;
+}
 document
   .getElementById("first_donate_btn")
   .addEventListener("click", function () {
@@ -24,6 +28,17 @@ document
 
     document.getElementById("in_total_amount").innerText = decriseIntotal;
     document.getElementById("amount").innerText = donate;
+
+    // donation History
+    const donationTime = new Date();
+    const donationPlace = donationPlaceEl("first_place_name");
+    const transcation = document.getElementById("history");
+    transcation.innerHTML += `
+    <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+    <p class='font-bold text-xl'>${inputField} Taka is  is ${donationPlace}</p>
+    <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+    </div>
+    `;
   });
 
 document
@@ -39,6 +54,17 @@ document
 
     document.getElementById("in_total_amount").innerText = decriseIntotal;
     document.getElementById("second_amount").innerText = donate;
+
+    // donation History
+    const donationTime = new Date();
+    const donationPlace = donationPlaceEl("second_place_name");
+    const transcation = document.getElementById("history");
+    transcation.innerHTML += `
+     <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+     <p class='font-bold text-xl'>${inputField} Taka is  is Donated for ${donationPlace}</p>
+     <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+     </div>
+     `;
   });
 
 document
@@ -54,4 +80,44 @@ document
 
     document.getElementById("in_total_amount").innerText = decriseIntotal;
     document.getElementById("third_amount").innerText = donate;
+    // donation History
+    const donationTime = new Date();
+    // console.log(donationTime);
+    const donationPlace = donationPlaceEl("third_place_name");
+    const transcation = document.getElementById("history");
+    transcation.innerHTML += `
+     <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+     <p class='font-bold text-xl'>${inputField} Taka is  is Donated for ${donationPlace}</p>
+     <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+     </div>
+     `;
+  });
+
+// donation and History
+
+function showbtn(id) {
+  document.getElementById("history").classList.add("hidden");
+  document.getElementById("donation").classList.add("hidden");
+
+  document.getElementById(id).classList.remove("hidden");
+}
+function showbtnColor(id) {
+  document.getElementById("history_add_btn").classList.remove("bg-lime-300");
+  document.getElementById("donation_add_btn").classList.remove("bg-lime-300");
+
+  document.getElementById(id).classList.add("bg-lime-300");
+}
+
+document
+  .getElementById("donation_add_btn")
+  .addEventListener("click", function () {
+    showbtn("donation");
+    showbtnColor("donation_add_btn");
+  });
+
+document
+  .getElementById("history_add_btn")
+  .addEventListener("click", function () {
+    showbtn("history");
+    showbtnColor("history_add_btn");
   });
